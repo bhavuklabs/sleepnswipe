21e9a2b8add17344f2dbb9cf32ee32aed843eaa9
chocoanish <chocoboyanish566@gmail.com>
2024-11-29 02:21:24 +0530

    Sidebar: Done

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import queMark from "../../assets/images/questionMarkIcon.png";
 import {
-  Home,
+  Heart,
+  LayoutDashboard,
+  Trophy,
+  Users,
+  Award,
+  Coins,
+  User,
   Settings,
-  LogOut,
+  FileText,
+  CreditCard,
+  Rocket,
 } from "lucide-react";
-import { Rewards, Community, Leaderboard, Tokens, MZone  } from "../../assets/images";
 import styles from "./Sidebar.module.css";
 
 interface SidebarButtonProps {
@@ -15,8 +23,8 @@ interface SidebarButtonProps {
 const SidebarButton: React.FC<SidebarButtonProps> = ({ icon, text }) => {
   return (
     <button className={styles.sidebarButton}>
-      {typeof icon === 'string' ? (
-        <img src={icon} alt=""/>
+      {typeof icon === "string" ? (
+        <img src={icon} alt="" />
       ) : (
         <span>{icon}</span>
       )}
@@ -33,49 +41,57 @@ const Sidebar: React.FC = () => {
       </div>
 
       {/* Divider Line */}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="100%"
-        height="2"
-        viewBox="0 0 234 1"
-        fill="none"
-      >
-        <path d="M0 0.5H233.25" stroke="url(#paint0_linear_7_100)" />
-        <defs>
-          <linearGradient
-            id="paint0_linear_7_100"
-            x1="0"
-            y1="0.5"
-            x2="231"
-            y2="0.5"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="#020515" stopOpacity="0.2" />
-            <stop offset="0.5" stopColor="#020515" />
-            <stop offset="1" stopColor="#020515" stopOpacity="0.2" />
-          </linearGradient>
-        </defs>
-      </svg>
+      <div className="divider">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="100%"
+          height="2"
+          viewBox="0 0 234 1"
+          fill="none"
+        >
+          <path d="M0 0.5H233.25" stroke="url(#paint0_linear_7_100)" />
+          <defs>
+            <linearGradient
+              id="paint0_linear_7_100"
+              x1="0"
+              y1="0.5"
+              x2="231"
+              y2="0.5"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#020515" stopOpacity="0.2" />
+              <stop offset="0.5" stopColor="#020515" />
+              <stop offset="1" stopColor="#020515" stopOpacity="0.2" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
 
       {/* Navigation Buttons */}
       <nav className={styles.nav}>
         <div className={styles.buttonGroup}>
-          <SidebarButton icon={<Home />} text="Dashboard" />
-          <SidebarButton icon={MZone} text="Match Zone" />
-          <SidebarButton icon={Leaderboard} text="Challenges" />
-          <SidebarButton icon={Community} text="Community Hub" />
-          <SidebarButton icon={Rewards} text="Rewards" />
-          <SidebarButton icon={Tokens} text="Token Store" />
+          <SidebarButton icon={<LayoutDashboard />} text="Dashboard" />
+          <SidebarButton icon={<Heart />} text="Match Zone" />
+          <SidebarButton icon={<Trophy />} text="Challenges" />
+          <SidebarButton icon={<Users />} text="Community Hub" />
+          <SidebarButton icon={<Award />} text="Rewards" />
+          <SidebarButton icon={<Coins />} text="Token Store" />
         </div>
 
         {/* Account Settings Section */}
         <div className={styles.accountSettings}>
           <p className={styles.accountSettingsText}>Account Settings</p>
+          <SidebarButton icon={<User />} text="Profile" />
+          <SidebarButton icon={<FileText />} text="Reports" />
           <SidebarButton icon={<Settings />} text="Settings" />
-          <SidebarButton icon={<LogOut />} text="Logout" />
-          <SidebarButton icon={<LogOut />} text="Logout" />
-          <SidebarButton icon={<LogOut />} text="Logout" />
-          <SidebarButton icon={<LogOut />} text="Logout" />
+          <SidebarButton icon={<CreditCard />} text="Billing" />
+          <SidebarButton icon={<Rocket />} text="Upgrade to Pro" />
+        </div>
+        <div className={styles.learnCard}>
+          <img src={queMark} alt="" />
+          <span>Need help?</span>
+          <p>Learn how to sleep better</p>
+          <button>Watch Our Guide</button>
         </div>
       </nav>
     </div>
